411566e6f75402d3408bfab91bebee8ccd167fb9
Mateusz Cieplak <m.cieplak97@gmail.com>
2025-11-14 10:25:07 +0100

    feat: Initial setup with React/TS/Vite and basic Tailwind config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+    <div className="bg-green-700 min-h-screen max-w-[1024px] mx-auto px-5"> 
+      <h1 className='font-extrabold'>Test</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
